3997059e8197583ea97f8ae388a51f3b379d1fed
Alec Philavong <alecphil@live.com>
2024-05-06 14:57:23 -0500

    added result messages and update room total math

--- scripts/index.js
+++ scripts/index.js
@@ -18,14 +18,15 @@ function customizeStay(event) {
     let priceQuoteForm = event.target
 
     //room types of stay
-    let typeOfStay = 150 * Number(priceQuoteForm.numOfNights.value);
+    let roomTotal = Number(priceQuoteForm.numOfNights.value) * getRoomRate(priceQuoteForm.checkInDate.avlue, priceQuoteForm.roomType.value);
 
     //let queennightStay = 150 * Number(priceQuoteForm.numOfNights.value);
     //let kingnightStay = 150 * Number(priceQuoteForm.numOfNights.value);
     //let twoBedRoomightStay = 150 * Number(priceQuoteForm.numOfNights.value);
 
-    //tax amount 
-    let taxAmount = typeOfStay * (12 / 100);
+    //number of nights stayed at 
+    let numOfNights = priceQuoteForm.numOfNights.value
+
 
     //discounts
     let discounts = 0;
@@ -44,26 +45,41 @@ function customizeStay(event) {
         discounts = 20 / 100;
     }
 
+    //number of nights stayed 
+    if (numOfNights >= 1) {
+
+    }
+
+   //tax amount 
+   let discountRoom = roomTotal * discounts
+   let taxAmount = discountRoom *(12/100)
 
 
 
 
+    function getRoomRate(checkInDate, roomType) {
 
+        if (roomType === "2BedRoom") {
 
-    function getRoomRate() {
+            return = 250
+        }
 
 
     }
 
 
-   
+
 
 }
 
 //total due 
-let totalDue = typeOfStay + discounts + taxAmount;
+let totalDue = roomTotal + discounts + taxAmount;
 
-let message= `
+let message = `
+<div class="mt-3"> Orginal Room Cost: ${roomTotal.tofixed(2)} </div>
+<div class="mt-3"> Discount value: $${discounts.tofixed(2)} </div>
+<div class="mt-3"> Discount room cost: $${discountRoom.tofixed(2)} </div>
+<div class="mt-3"> Tax Amount: $${taxAmount.tofixed(2)} </div>
 <div class="mt-3"> totalDue: ${totalDue.tofixed(2)} </div>
 `
 
